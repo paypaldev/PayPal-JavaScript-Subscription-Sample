dd3cd4db3342be4bcea4cebc3606b21d0a6a71b3
Pato <11162114+devpato@users.noreply.github.com>
2023-09-07 12:37:23 -0500

    Update client/app.js
    Co-authored-by: Greg Jopa <534034+gregjopa@users.noreply.github.com>

--- client/app.js
+++ client/app.js
@@ -38,7 +38,7 @@ window.paypal
     onApprove(data) {
       /*
         No need to activate manually since  SUBSCRIBE_NOW is being used.
-        Leanr how to handle other user actions from our docs:
+        Learn how to handle other user actions from our docs:
         https://developer.paypal.com/docs/api/subscriptions/v1/#subscriptions_create
       */
       if (data.orderID) {
